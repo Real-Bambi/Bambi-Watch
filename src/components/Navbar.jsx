dbc94e44773c953e2bfe6fd16562aa930502e18d
Sally <salomeytackie555@gmail.com>
2025-07-09 16:52:52 +0000

    creating the Nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,46 @@
+import { Play, Menu, X } from "lucide-react";
+import { Link } from "react-router";
+import { useState } from "react";
+
+
+
 export default function Navbar() {
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+
+
   return (
-    <footer className="bg-gray-800 text-white py-4">
-      <div className="container mx-auto text-center">
-        <p className="text-sm">
-          &copy; {new Date().getFullYear()} BambiWatch. All rights reserved.
-        </p>
-        <p className="text-xs mt-2">
-          Made with ❤️ by the BambiWatch Team
-        </p>
-      </div>
-    </footer>
+   <div className="flex flex-row justify-between items-center p-2">
+    <div className="flex flex-row">
+      <Play />
+      <h1 className=" text-lg md:text-2xl font-bold">BambiWatch</h1>
+    </div>
+    {/* Desktop Menu */}
+    <div className="hidden md:flex flex-row justify-between gap-6">
+    <Link to={'#'}>Home</Link>
+    <Link to={'#'}>About</Link>
+    <Link to={'#'}>Reviews</Link>
+    <Link to={'#'}>Contact</Link>
+    </div>
+
+    {/* Mobile Menu */}
+    {mobileMenu && (
+    <div className=" md:hidden flex flex-col bg-white justify-between gap-6 fixed top-12 right-0">
+    <Link to={'#'}>Home</Link>
+    <Link to={'#'}>About</Link>
+    <Link to={'#'}>Reviews</Link>
+    <Link to={'#'}>Contact</Link>
+    </div>
+    )}
+    <div className="flex justify-between gap-2 items-center">
+      <Link to={'#'}>Sign In</Link>
+      <Link to={'#'}><button className="bg-[#9333EA] px-6 py-1.5  text-white rounded-xl">Get Started</button></Link>
+    </div>
+
+    {/* Menu Icon */}
+    <div className="md:hidden" >
+      <button className="md:hidden " onClick={() =>setMobileMenu(!mobileMenu)}> {mobileMenu ? <X /> : <Menu />}</button>
+    </div>
+   </div>
   );
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,16 @@
+import Navbar from "../components/Navbar";
+
+
 export default function Profile() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    
+  <div>
+    <Navbar />
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold mb-4">Profile</h1>
       <p className="text-lg text-gray-700">Manage your profile settings here.</p>
       <p className="text-sm text-gray-500 mt-2">Update your information and preferences.</p>
     </div>
+  </div>
   );
 }
