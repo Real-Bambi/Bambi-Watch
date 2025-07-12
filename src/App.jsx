e045d78b9039dda6ae3b1a40429c8ffb343aa358
Dede <dedenyengor09@gmail.com>
2025-07-12 16:21:59 +0000

    dede firdt commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
@@ -11,6 +12,7 @@ const bambiWatchRouter = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/register", element: <Register /> },
   { path: "/login", element: <Login /> },
+  { path: "/sign-up", element: <SignUp /> },
   { path: "/dashboard", element: <Dashboard /> },
   { path: "/profile", element: <Profile /> },
   { path: "/watchroom", element: <Watchroom /> },
